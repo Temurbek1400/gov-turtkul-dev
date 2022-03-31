751b0da5b8c2daaf1f1a37fcc343f855becd23e2
Saidmuhammad <sololearner567@gmail.com>
2022-03-31 22:37:47 +0500

    saidmuhammad first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import LatestNews from "./components/latest-news/latest-news";
 
 function App() {
-   return <div className="App">Hello 906</div>;
+  return (
+    <div className="App">
+      Hello 906
+      <LatestNews />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/latest-news/latest-news.jsx
@@ -0,0 +1,7 @@
+import { Title } from "./latest-news.style";
+
+const LatestNews = () => {
+  return <Title>LatestNews</Title>;
+};
+
+export default LatestNews;
