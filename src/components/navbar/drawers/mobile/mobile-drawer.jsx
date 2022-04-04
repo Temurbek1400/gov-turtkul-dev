687d7ab5da3087748fa0a883464545913a893337
Temurbek Rajapboyev <rajapboyevtemurbek@gmail.com>
2022-04-04 14:57:30 +0500

    app bar is done

--- src/components/navbar/drawers/mobile/mobile-drawer.jsx
+++ src/components/navbar/drawers/mobile/mobile-drawer.jsx
@@ -1,12 +1,6 @@
-import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import ListItem from "@mui/material/ListItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
 import IconButton from "@mui/material/IconButton";
 import { useTheme } from "@mui/material/styles";
 import { DrawerMobile } from "./mobile-drawer.styles";
@@ -19,7 +13,6 @@ export default function MobileDrawer({
    navbarData,
 }) {
    const theme = useTheme();
-   console.log(console.count());
    return (
       <DrawerMobile variant="persistent" anchor="left" open={drawer}>
          <DrawerHeader>
