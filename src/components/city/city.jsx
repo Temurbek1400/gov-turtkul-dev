1130e685abcd61df159c6e673e6bdf5931f65e52
mirjahon <mirjahonoralov@gmail.com>
2022-04-05 07:30:11 +0500

    city animation

--- src/components/city/city.jsx
+++ src/components/city/city.jsx
@@ -2,24 +2,33 @@ import { CardText } from "components/events/events.style";
 import { CardWrapper } from "components/events/events.style";
 import { Wrapper } from "components/events/events.style";
 import Title from "components/title";
-import React from "react";
+import React, { useEffect } from "react";
 import { CityData } from "./city-data";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const City = () => {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+
   return (
     <div style={{ marginBottom: "30px" }}>
       <Title text="Shahar" />
-      <Wrapper>
-        {CityData.map((item, id) => {
-          return (
-            <CardWrapper key={id}>
-              <img src={item.img} alt="event-card-img" />
-              <CardText>{item.title}</CardText>
-              <p>{item.desc}</p>
-            </CardWrapper>
-          );
-        })}
-      </Wrapper>
+      <div data-aos="fade-up">
+        <Wrapper>
+          {CityData.map((item, id) => {
+            return (
+              <CardWrapper key={id}>
+                <img src={item.img} alt="event-card-img" />
+                <CardText>{item.title}</CardText>
+                <p>{item.desc}</p>
+              </CardWrapper>
+            );
+          })}
+        </Wrapper>
+      </div>
     </div>
   );
 };
