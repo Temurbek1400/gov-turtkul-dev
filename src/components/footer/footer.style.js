cd37450fe13369906c61a2e18273fe939016f969
mirjahon <mirjahonoralov@gmail.com>
2022-04-05 11:29:47 +0500

    change footer

--- src/components/footer/footer.style.js
+++ src/components/footer/footer.style.js
@@ -4,17 +4,47 @@ export const Wrapper = styled.div`
   margin: 20px -30px 0;
   background-color: ${({ theme }) => theme.primary};
   padding: 30px 30px 50px;
+  color: #fff;
+  position: relative;
+
+  @media (max-width: 900px) {
+    text-align: center;
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.mobileL}) {
+    margin: 20px -20px 0;
+    padding: 30px 30px 60px;
+  }
+`;
+
+export const FooterTop = styled.div`
   display: flex;
-  justify-content: space-between;
+
+  @media (max-width: 900px) {
+    flex-direction: column;
+  }
 `;
 
 export const Left = styled.div`
-  width: 32%;
-  color: #fff;
+  width: 70%;
+  display: flex;
+  gap: 30px;
+
+  p {
+    opacity: 0.75;
+  }
+
+  @media (max-width: 900px) {
+    width: 100%;
+    align-items: center;
+    flex-direction: column;
+    gap: 0;
+    text-align: center;
+  }
 `;
 
 export const ImgWrapper = styled.div`
-  width: 20%;
+  width: 100px;
 
   img {
     width: 100%;
@@ -23,7 +53,6 @@ export const ImgWrapper = styled.div`
 
 export const LeftContent = styled.div`
   display: flex;
-  justify-content: space-between;
   align-items: center;
   margin-bottom: 30px;
   p {
@@ -33,6 +62,23 @@ export const LeftContent = styled.div`
 
 export const Right = styled.div`
   width: 30%;
-  color: #fff;
   line-height: 150%;
+
+  @media (max-width: 900px) {
+    width: 100%;
+    text-align: center;
+  }
+`;
+
+export const FooterBottom = styled.div`
+  p {
+    opacity: 0.75;
+    margin-top: 0;
+  }
+  img {
+    position: absolute;
+    width: 100px;
+    right: 30px;
+    bottom: 30px;
+  }
 `;
