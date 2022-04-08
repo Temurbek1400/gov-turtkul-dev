57ae1fd545e846b2586d4b0db2205ff1c5fc424c
Saidmuhammad <sololearner567@gmail.com>
2022-04-08 19:07:09 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Loader from "components/common/loader";
 import { Suspense } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "routes/config";
 
 function App() {
   return (
-    <Suspense fallback="Loading...">
+    <Suspense fallback={<Loader />}>
       <Router>
         <Routes />
       </Router>
