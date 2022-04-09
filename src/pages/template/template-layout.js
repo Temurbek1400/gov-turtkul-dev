298e44c0913e27ad47f5331ed7b3a7a746416c0f
Saidmuhammad <sololearner567@gmail.com>
2022-04-09 20:43:19 +0500

    latest route changes

--- src/pages/template/template-layout.js
+++ src/pages/template/template-layout.js
@@ -9,10 +9,10 @@ const TemplateLayout = () => {
     <div>
       <BreadCrumbs />
       <Grid container spacing={4}>
-        <Grid item md={8}>
+        <Grid item sm={7} md={8}>
           <Outlet />
         </Grid>
-        <Grid item md={4}>
+        <Grid item sm={5} md={4}>
           <LeftContent />
         </Grid>
       </Grid>
