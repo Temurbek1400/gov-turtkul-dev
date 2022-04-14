5746070ef7ae5bd110b9df2c0a8027000c2b29e7
mirjahon <mirjahonoralov@gmail.com>
2022-04-14 23:42:02 +0500

    adaptive news

--- src/components/filtered-news/filtered-news-items.jsx
+++ src/components/filtered-news/filtered-news-items.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { NewsItemWrapper, NewsInfo, NewsWrapper } from "./filtered-news.styles";
 import Grid from "@mui/material/Grid";
-import { Divider } from "@mui/material";
 import { Link } from "react-router-dom";
 import NotFound from "components/not-found/not-found";
 import { useSelector } from "react-redux";
 import { getActiveFilter } from "store/reducer-and-action/news/newsSlice";
+import EventIcon from "@mui/icons-material/Event";
 
 const FilteredNewsItems = ({ news = [] }) => {
   const newsFilter = useSelector(getActiveFilter);
@@ -15,21 +15,25 @@ const FilteredNewsItems = ({ news = [] }) => {
         news.map((news) => (
           <Link to={`/news/${newsFilter}/${news.id}`} key={news.id}>
             <NewsItemWrapper role="tabpanel">
-              <Grid container key={news.title} columnSpacing={2}>
-                <Grid item xs={12} sm={6} md={4} lg={3}>
+              <Grid
+                container
+                key={news.title}
+                columnSpacing={2}
+                style={{ display: "flex", alignItems: "center" }}
+              >
+                <Grid item xs={12} sm={6} md={6} lg={6}>
                   <img src={news.images[0]} alt="" />
                 </Grid>
-                <Grid item xs={12} sm={6} md={8} lg={9}>
+                <Grid item xs={12} sm={6} md={6} lg={6}>
                   <NewsInfo>
-                    <h2>{news.title}</h2>
+                    <h4>{news.title}</h4>
                     <p>{news.body}</p>
-                    <p>{news.date}</p>
+                    <h5>
+                      <EventIcon /> {news.date}
+                    </h5>
                   </NewsInfo>
                 </Grid>
               </Grid>
-              <br />
-              <Divider />
-              <br />
             </NewsItemWrapper>
           </Link>
         ))
--- src/components/filtered-news/filtered-news.styles.js
+++ src/components/filtered-news/filtered-news.styles.js
@@ -26,23 +26,80 @@ export const NewsWrapper = styled.div`
     text-decoration: none;
     color: ${(props) => props.theme.textColor};
   }
-  /* width: 100%;
-   @media screen and (max-width: 768px) {
-      width: 100%;
-   } */
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 50px;
+  row-gap: 20px;
+  margin: 20px 0;
+
+  @media screen and (max-width: ${({ theme }) => theme.media.laptop}) {
+    column-gap: 20px;
+    grid-template-columns: 1fr;
+  }
+
+  @media screen and (max-width: 600px) {
+    display: flex;
+    flex-direction: column;
+  }
 `;
 export const NewsItemWrapper = styled.div`
-  /* display: flex;
-   justify-content: space-between;*/
+  border-bottom: 1px solid silver;
+  padding-bottom: 20px;
   & img {
     width: 100%;
   }
-  /*@media screen and (max-width: 768px) {
-   } */
+
+  :hover {
+    color: #000;
+  }
 `;
 export const NewsInfo = styled.div`
   margin-left: 20px;
-  & h2 {
+  & h4 {
     color: ${(props) => props.theme.primary};
+    margin: 0;
+  }
+
+  h5 {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    margin: 0;
+    svg {
+      font-size: 20px;
+    }
+  }
+
+  p {
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+  }
+
+  @media screen and (max-width: ${({ theme }) => theme.media.laptop}) {
+    p {
+      display: none;
+    }
+
+    h4 {
+      margin-bottom: 20px;
+    }
+  }
+
+  @media screen and (max-width: ${({ theme }) => theme.media.laptop}) {
+    p {
+      display: -webkit-box;
+      -webkit-line-clamp: 3;
+      -webkit-box-orient: vertical;
+      overflow: hidden;
+    }
+  }
+  @media screen and (max-width: 600px) {
+    margin-left: 0;
+    h4 {
+      margin-top: 15px;
+    }
   }
 `;
