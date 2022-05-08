abe67ff50796f2cbb75a412abb50b48d002e6e00
mirjahon <mirjahonoralov@gmail.com>
2022-05-08 14:27:25 +0500

    changed

--- src/components/filtered-news/filtered-news-items.jsx
+++ src/components/filtered-news/filtered-news-items.jsx
@@ -10,55 +10,47 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 const FilteredNewsItems = ({ news = [] }) => {
-   const newsFilter = useSelector(getActiveFilter);
+  const newsFilter = useSelector(getActiveFilter);
 
-   useEffect(() => {
-      AOS.init();
-      AOS.refresh();
-   }, []);
-   return (
-      <NewsWrapper>
-         {news.length ? (
-            news.map((news_item) => (
-               <div data-aos="fade-right">
-                  <Link
-                     to={`/news/${newsFilter}/${news_item.id}`}
-                     key={news_item.id}
-                  >
-                     <NewsItemWrapper role="tabpanel">
-                        <Grid
-                           container
-                           key={news_item.title}
-                           columnSpacing={2}
-                           style={{ display: "flex", alignItems: "center" }}
-                        >
-                           <Grid item xs={12} sm={6} md={6} lg={6}>
-                              <img
-                                 src={
-                                    news_item.imagesown
-                                 }
-                                 alt=""
-                              />
-                           </Grid>
-                           <Grid item xs={12} sm={6} md={6} lg={6}>
-                              <NewsInfo>
-                                 <h4>{news_item.title}</h4>
-                                 <p>{news_item.body}</p>
-                                 <h5>
-                                    <EventIcon /> {news_item.date}
-                                 </h5>
-                              </NewsInfo>
-                           </Grid>
-                        </Grid>
-                     </NewsItemWrapper>
-                  </Link>
-               </div>
-            ))
-         ) : (
-            <NotFound />
-         )}
-      </NewsWrapper>
-   );
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+  return (
+    <NewsWrapper>
+      {news.length ? (
+        news.map((news_item) => (
+          <div data-aos="fade-right">
+            <Link to={`/${newsFilter}/${news_item.id}`} key={news_item.id}>
+              <NewsItemWrapper role="tabpanel">
+                <Grid
+                  container
+                  key={news_item.title}
+                  columnSpacing={2}
+                  style={{ display: "flex", alignItems: "center" }}
+                >
+                  <Grid item xs={12} sm={6} md={6} lg={6}>
+                    <img src={news_item.imagesown} alt="" />
+                  </Grid>
+                  <Grid item xs={12} sm={6} md={6} lg={6}>
+                    <NewsInfo>
+                      <h4>{news_item.title}</h4>
+                      <p>{news_item.body}</p>
+                      <h5>
+                        <EventIcon /> {news_item.date}
+                      </h5>
+                    </NewsInfo>
+                  </Grid>
+                </Grid>
+              </NewsItemWrapper>
+            </Link>
+          </div>
+        ))
+      ) : (
+        <NotFound />
+      )}
+    </NewsWrapper>
+  );
 };
 
 export default FilteredNewsItems;
