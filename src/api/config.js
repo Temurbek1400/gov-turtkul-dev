8c55cd5529ab2fb04a615ca5b2bfe34fad4629c7
Temurbek Rajapboyev <rajapboyevtemurbek@gmail.com>
2022-06-05 15:32:01 +0500

    ok

--- src/api/config.js
+++ src/api/config.js
@@ -3,9 +3,5 @@ export const BASE_URL = "https://turtkul3.herokuapp.com";
 
 export default axios.create({
   baseURL: BASE_URL,
-  mode: "no-cors",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
+  withCredentials: false,
 });
