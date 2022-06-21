ed0beb604f17fce2ffe83a300c99fc85e528b37f
Temurbek Rajapboyev <rajapboyevtemurbek@gmail.com>
2022-06-21 08:46:15 +0500

    ok

--- src/components/header/header-news-item.jsx
+++ src/components/header/header-news-item.jsx
@@ -1,22 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import formatDate from "utils/formatDate";
 import { HeaderNewsItemWrapper } from "./header.styles";
 
 const HeaderNewsItem = ({ news }) => {
   return (
     <>
-      <HeaderNewsItemWrapper>
-        <figure>
-          <img src={news?.imagesown} alt="news" />
-          <figcaption>
-            <p>{"Batafsil" || "More..."}</p>
-          </figcaption>
-        </figure>
-        <div className="card-info">
-          <p>{news?.title}</p>
-          <p>{formatDate(news?.date)}</p>
-        </div>
-      </HeaderNewsItemWrapper>
+      {/* <Link
+        to={
+          newsFilter
+            ? `/${newsFilter}/${news_item.id}`
+            : `/news/${news_item.filter}/${news_item.id}`
+        }
+        key={news_item.id}
+      > */}
+        <HeaderNewsItemWrapper>
+          <figure>
+            <img src={news?.imagesown} alt="news" />
+            <figcaption>
+              <p>{"Batafsil" || "More..."}</p>
+            </figcaption>
+          </figure>
+          <div className="card-info">
+            <p>{news?.title}</p>
+            <p>{formatDate(news?.date)}</p>
+          </div>
+        </HeaderNewsItemWrapper>
+      {/* </Link> */}
     </>
   );
 };
