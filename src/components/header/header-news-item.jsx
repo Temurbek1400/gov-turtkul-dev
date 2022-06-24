5d6abc9f13193e05b60c67ad24d0bc7cc8d93c75
Temurbek Rajapboyev <rajapboyevtemurbek@gmail.com>
2022-06-24 22:44:47 +0500

    ok

--- src/components/header/header-news-item.jsx
+++ src/components/header/header-news-item.jsx
@@ -3,30 +3,27 @@ import { Link } from "react-router-dom";
 import formatDate from "utils/formatDate";
 import { HeaderNewsItemWrapper } from "./header.styles";
 
-const HeaderNewsItem = ({ news }) => {
+const HeaderNewsItem = ({ news: news_item }) => {
+  debugger;
   return (
     <>
-      {/* <Link
-        to={
-          newsFilter
-            ? `/${newsFilter}/${news_item.id}`
-            : `/news/${news_item.filter}/${news_item.id}`
-        }
-        key={news_item.id}
-      > */}
+      <Link
+        to={`/news/${news_item?.filter}/${news_item?.id}`}
+        key={news_item?.id}
+      >
         <HeaderNewsItemWrapper>
           <figure>
-            <img src={news?.imagesown} alt="news" />
+            <img src={news_item?.imagesown} alt="news" />
             <figcaption>
               <p>{"Batafsil" || "More..."}</p>
             </figcaption>
           </figure>
           <div className="card-info">
-            <p>{news?.title}</p>
-            <p>{formatDate(news?.date)}</p>
+            <p>{news_item?.title}</p>
+            <p>{formatDate(news_item?.date)}</p>
           </div>
         </HeaderNewsItemWrapper>
-      {/* </Link> */}
+      </Link>
     </>
   );
 };
